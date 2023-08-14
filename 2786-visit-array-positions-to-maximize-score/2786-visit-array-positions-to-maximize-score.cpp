0dc0aff65d9dcb9c51498166df85a1045466d751
gargsujal1 <101945632+gargsujal1@users.noreply.github.com>
2023-08-14 19:21:04 +0530

    Time: 122 ms (99.50%), Space: 99.4 MB (93.13%) - LeetHub

--- 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
+++ 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
-    long long maximumscore(vector<int>& nums,int x,int i,int parity,vector<vector<long long>> &dp){
-        if(i==nums.size()){
-            return 0;
-        }
-        if(dp[parity][i]!=-1){
-            return dp[parity][i];
-        }
-        int z=nums[i]%2;
-        if(z!=parity){
-            long long pick=nums[i]-x+maximumscore(nums,x,i+1,z,dp);
-            long long notpick=maximumscore(nums,x,i+1,parity,dp);
-            return dp[parity][i]=max(pick,notpick);
-        }
-        else{
-            return dp[parity][i]=nums[i]+maximumscore(nums,x,i+1,parity,dp);
-        }
-        return 0;
-    }
-    long long maxScore(vector<int>& nums, int x) {
-        int parity=nums[0]%2;
-        vector<vector<long long>> dp(2,vector<long long>(nums.size(),-1));
-        return nums[0]+maximumscore(nums,x,1,parity,dp);
-    }
+    long long maxScore(vector<int>& n, int x) {
+    long long eve = n[0] - (n[0] % 2 ? x : 0);
+    long long odd = n[0] - (n[0] % 2 ? 0 : x);
+    for (int i = 1; i < n.size(); ++i)
+        if (n[i] % 2)
+            odd = n[i] + max(odd, eve - x);
+        else
+            eve = n[i] + max(eve, odd - x);
+    return max(eve, odd);
+}
 };
