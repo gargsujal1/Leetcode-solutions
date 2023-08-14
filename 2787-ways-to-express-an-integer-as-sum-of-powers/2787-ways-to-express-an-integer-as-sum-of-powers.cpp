d845a745e18b6e811ddf8e9da30f9ca60f1387e6
gargsujal1 <101945632+gargsujal1@users.noreply.github.com>
2023-08-15 00:08:43 +0530

    Time: 418 ms (60.55%), Space: 382.1 MB (15.84%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-     int mod = 1e9+7;
-     int dp[301][301];
-    
-     int f(int n, int num, int x ) {
-            if(n < 0 )return 0; 
-            if(n == 0)return 1; 
-            if(pow(num , x) > n) return 0;
-            if(dp[n][num] != -1) return dp[n][num];
-          
-            int temp = pow(num , x) ;
-            
-            int pick = f(n- temp , num + 1 , x);
-            int skip = f(n , num + 1 , x);
-            
-            return dp[n][num] =  (skip % mod + pick % mod) % mod ;
+    const int mod=1e9+7;
+    int waysToExpress(int n,int i,int x,vector<vector<int>>& dp){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        if(pow(i,x)>n){
+            return 0;
+        }
+        if(dp[n][i]!=-1){
+            return dp[n][i];
+        }
+        //First lets calculate the value of current i to power x
+        int currval=pow(i,x);
+        //Now i can either take the element in my current combination or skip it
+        int take=waysToExpress(n-currval,i+1,x,dp);
+        int skip=waysToExpress(n,i+1,x,dp);
+        
+        return dp[n][i]=(take%mod + skip%mod)%mod;
+        
     }
-    
-    
     int numberOfWays(int n, int x) {
-        memset(dp, -1, sizeof(dp));
-        return f(n, 1, x);
+        vector<vector<int>> dp(301,vector<int>(301,-1));
+        return waysToExpress(n,1,x,dp);
     }
 };
