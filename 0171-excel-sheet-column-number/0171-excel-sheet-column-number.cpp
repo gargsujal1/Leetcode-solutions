951a9408fbf8cc6ff13a8d3fb2212676234a9eec
gargsujal1 <101945632+gargsujal1@users.noreply.github.com>
2023-08-21 15:55:10 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (18.14%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -4,6 +4,10 @@ public:
         int ans=0;
         int i=0;
         int l=columnTitle.size();
+        //AAA
+        //To calculate first find value of corresponding letter and also rem len ahead
+        //then add pow(26,rem)*value of thar letter
+        //do this for all the characters from start to end.
         while(i<l){
             int rem=l-i-1;
             rem=pow(26,rem);
