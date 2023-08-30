91ce3bbfa8dbd9bca4d2af9f6ec01754ef197469
gargsujal1 <101945632+gargsujal1@users.noreply.github.com>
2023-08-30 13:52:33 +0530

    Time: 290 ms (52.49%), Space: 153 MB (46.66%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -19,8 +19,27 @@ public:
         notpick=sequence(nums,i+1,j,dp);
         return dp[i][j]=max(pick,notpick);
     }
+    long long sequence2(vector<int>& nums){
+        int n=nums.size();
+        vector<vector<long long>> dp(nums.size()+1,vector<long long>(2,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                long long pick=0;
+        long long notpick=0;
+        if(j==0){
+            pick=nums[i]+dp[i+1][1];
+        }
+        else if(j==1){
+            pick=dp[i+1][0]-nums[i];
+        }
+        notpick=dp[i+1][j];
+        dp[i][j]=max(pick,notpick);
+            }
+        }
+        return dp[0][0]; 
+    }
     long long maxAlternatingSum(vector<int>& nums) {
-        vector<vector<long long>> dp(nums.size(),vector<long long>(2,-1));
-        return sequence(nums,0,0,dp);
+        // vector<vector<long long>> dp(nums.size(),vector<long long>(2,-1));
+        return sequence2(nums);
     }
 };
