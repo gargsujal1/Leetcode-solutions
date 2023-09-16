65e02c66faa81e4e12e542175d884a26d7430476
gargsujal1 <101945632+gargsujal1@users.noreply.github.com>
2023-09-16 15:47:57 +0530

    Time: 36 ms (5.09%), Space: 16.5 MB (5.60%) - LeetHub

--- 1034-coloring-a-border/1034-coloring-a-border.cpp
+++ 1034-coloring-a-border/1034-coloring-a-border.cpp
@@ -1,50 +1,61 @@
 class Solution {
 public:
-    bool isVisited[51][51];
-    vector<vector<int>> result;
-    int ROW, COL, COLOR;
-    
-    bool isValid(vector<vector<int>>& grid, int i, int j){
-        if((i>=0 and i<ROW) and (j>=0 and j<COL) and (grid[i][j] == COLOR) and (!isVisited[i][j]))
-            return true;
+    int dx[4]={-1,0,1,0};
+    int dy[4]={0,1,0,-1};
+    bool isValid(int x,int y,int n,int m){
+        if(x<0 || y<0 || x>=n || y>=m){
+            return false;
+        }
+        return true;
+    }
+    bool isBorder(vector<vector<int>>& grid,int x,int y,int n,int m){
+        for(int i=0;i<4;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(isValid(nx,ny,n,m)==true){
+                if(grid[nx][ny]!=grid[x][y]){
+                    return true;
+                }
+            }
+            else{
+                return true;
+            }
+        }
         return false;
     }
-    
-    bool isBorder(vector<vector<int>>& grid, int i, int j){
-        if(i == 0 || i == ROW-1 || j == 0 || j == COL-1)
-            return true;
-        if(grid[i][j+1] != COLOR || grid[i][j-1] != COLOR || grid[i+1][j] != COLOR || grid[i-1][j] != COLOR)
-            return true;
-        return false;
-    }
-    
-    void DFS(vector<vector<int>>& grid, int i, int j, int color){
-        isVisited[i][j] = true;
-        if(isBorder(grid, i, j))
-            result[i][j] = color;
-        if(isValid(grid, i, j+1)){
-            DFS(grid, i, j+1, color);
+    vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> visited(n,vector<int>(m,-1));
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        while(!q.empty()){
+            auto f=q.front();
+            q.pop();
+            int x=f.first;
+            int y=f.second;
+            bool border=isBorder(grid,x,y,n,m);
+            if(border==true){
+             visited[x][y]=2;   
+            }
+            else{
+                visited[x][y]=1;
+            }
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(isValid(nx,ny,n,m)==true && visited[nx][ny]==-1 && grid[nx][ny]==grid[x][y]){
+                    q.push({nx,ny});
+                }
+            }
         }
-        if(isValid(grid, i, j-1)){
-            DFS(grid, i, j-1, color);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visited[i][j]==2){
+                    grid[i][j]=color;
+                }
+            }
         }
-        if(isValid(grid, i+1, j)){
-            DFS(grid, i+1, j, color);
-        }
-        if(isValid(grid, i-1, j)){
-            DFS(grid, i-1, j, color);
-        }
-    }
-    
-    vector<vector<int>> colorBorder(vector<vector<int>>& grid, int r0, int c0, int color) {
-        memset(isVisited, false, sizeof isVisited);
-        ROW = grid.size(); COL = grid[0].size(); COLOR = grid[r0][c0];
-        result.resize(ROW, vector<int>(COL));
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++)
-                result[i][j] = grid[i][j];
-        }
-        DFS(grid, r0, c0, color);
-        return result;    
+        return grid;
     }
 };
