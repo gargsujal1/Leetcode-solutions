06bdb7e629e5ffff5171d48e30000b8fc3ce5472
gargsujal1 <101945632+gargsujal1@users.noreply.github.com>
2023-10-22 23:33:18 +0530

    Time: 113 ms (97.15%), Space: 89.7 MB (73.03%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     int maximumScore(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int> left(n,-1);
-        vector<int> right(n,-1);
-        stack<pair<int,int>> stk;
-        stk.push({nums[0],0});
-        for(int i=1;i<n;i++){
-            while(!stk.empty() && stk.top().first>nums[i]){
-                right[stk.top().second]=i-1;
-                stk.pop();
+        int n = nums.size();
+
+        int i = k;
+        int j = k;
+        int curMin = nums[k];
+        
+        int result = nums[k];
+        
+        while(i > 0 || j < n-1) {
+            int leftValue;
+            int rightValue;
+            
+            if(i > 0) {
+                leftValue = nums[i-1];
+            } else {
+                leftValue = 0;
             }
-            stk.push({nums[i],i});
-        }
-        while(!stk.empty()){
-            right[stk.top().second]=n-1;
-                stk.pop();
-        }
-        stk.push({nums[n-1],n-1});
-        for(int i=n-2;i>=0;i--){
-            while(!stk.empty() && stk.top().first>nums[i]){
-                left[stk.top().second]=i+1;
-                stk.pop();
+            
+            if(j < n-1) {
+                rightValue = nums[j+1];
+            } else {
+                rightValue = 0;
             }
-            stk.push({nums[i],i});
-        }
-        while(!stk.empty()){
-            left[stk.top().second]=0;
-                stk.pop();
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(k>=left[i] && k<=right[i]){
-                int z=nums[i]*(right[i]-left[i]+1);
-                ans=max(ans,z);
+            
+            if(leftValue < rightValue) { 
+                j++;
+                curMin = min(curMin, nums[j]);
+            } else {
+                i--;
+                curMin = min(curMin, nums[i]);
             }
+            
+            result = max(result, curMin * (j-i+1));
         }
-        return ans;
+        
+        return result;
     }
 };
